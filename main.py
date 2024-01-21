fb4cf814a8969d67122d30f813397a83f70af442
Serpyne <156378996+Serpyne@users.noreply.github.com>
2024-01-21 14:14:29 +0000

    main

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import bot
 import server
-import os
-import json
-import re
-
-# text = re.sub("url", server.site_url(), os.getenv("setup"))
-# print(text)
-# setup = json.loads(str(text))
 
 server.start()
-# bot.run(setup=setup, token=os.getenv("TOKEN"))
